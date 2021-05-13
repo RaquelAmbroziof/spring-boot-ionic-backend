10539d0db4e373cadd9d244cd46a9fea9cc1a859
Raquel Ambrozio <ambrozioraquel@gmail.com>
2021-05-13 14:25:15 -0300

    pedido, pagamento, estadoPagamento

--- src/main/java/com/raquelaf/cursomc/CursomcApplication.java
+++ src/main/java/com/raquelaf/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.raquelaf.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.raquelaf.cursomc.domain.Cidade;
 import com.raquelaf.cursomc.domain.Cliente;
 import com.raquelaf.cursomc.domain.Endereco;
 import com.raquelaf.cursomc.domain.Estado;
+import com.raquelaf.cursomc.domain.Pagamento;
+import com.raquelaf.cursomc.domain.PagamentoComBoleto;
+import com.raquelaf.cursomc.domain.PagamentoComCartao;
+import com.raquelaf.cursomc.domain.Pedido;
 import com.raquelaf.cursomc.domain.Produto;
+import com.raquelaf.cursomc.domain.enums.EstadoPagamento;
 import com.raquelaf.cursomc.domain.enums.TipoCliente;
 import com.raquelaf.cursomc.repositories.CategoriaRepository;
 import com.raquelaf.cursomc.repositories.CidadeRepository;
 import com.raquelaf.cursomc.repositories.ClienteRepository;
 import com.raquelaf.cursomc.repositories.EnderecoRepository;
 import com.raquelaf.cursomc.repositories.EstadoRepository;
+import com.raquelaf.cursomc.repositories.PagamentoRepository;
+import com.raquelaf.cursomc.repositories.PedidoRepository;
 import com.raquelaf.cursomc.repositories.ProdutoResopitory;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository cliRepo;
 	@Autowired
 	private EnderecoRepository endRepo;
+	@Autowired
+	private PedidoRepository pedRepo;
+	@Autowired
+	private PagamentoRepository pagRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,6 +96,29 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		cliRepo.saveAll(Arrays.asList(cli1));
 		endRepo.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedRepo.saveAll(Arrays.asList(ped1, ped2));
+		pagRepo.saveAll(Arrays.asList(pagto1, pagto2));
+
+		
+		
+		
+
+		
 	}
 
 }
---
+++ src/main/java/com/raquelaf/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,31 @@
+package com.raquelaf.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.raquelaf.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroParcelas) {
+		super(id, estado, pedido);
+		this.setNumeroParcelas(numeroParcelas);
+	}
+
+	public Integer getNumeroParcelas() {
+		return numeroParcelas;
+	}
+
+	public void setNumeroParcelas(Integer numeroParcelas) {
+		this.numeroParcelas = numeroParcelas;
+	}
+	
+	
+
+}
