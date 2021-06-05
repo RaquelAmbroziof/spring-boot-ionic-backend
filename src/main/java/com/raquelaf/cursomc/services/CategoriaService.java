36b5f40917f7688e178483f3d0e0e0a86f2e6a7c
Raquel Ambrozio <ambrozioraquel@gmail.com>
2021-06-05 12:23:54 -0300

    validacao sintatica com bean validation

--- src/main/java/com/raquelaf/cursomc/services/CategoriaService.java
+++ src/main/java/com/raquelaf/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.raquelaf.cursomc.domain.Categoria;
+import com.raquelaf.cursomc.dto.CategoriaDTO;
 import com.raquelaf.cursomc.repositories.CategoriaRepository;
 import com.raquelaf.cursomc.services.exception.DataIntegrityException;
 import com.raquelaf.cursomc.services.exception.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 
